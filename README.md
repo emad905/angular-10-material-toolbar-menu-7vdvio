d7622bb77f9e629f02b6e9d95d5603b040137081
emad905 <emadhesham1996@hotmail.com>
2022-01-15 15:37:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-10-material-toolbar-menu-7vdvio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-10-material-toolbar-menu-7vdvio)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
+
+import { Observable, of } from 'rxjs';
+
+interface IColor {
+  name: string;
+  value: string;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public colors: Array<IColor> = [{
+    name: 'Lime Green',
+    value: '#00ff00',
+  }, {
+    name: 'Red',
+    value: '#ff0000',
+  }, {
+    name: 'Blue',
+    value: '#0000ff',
+  }];
+
+  public profile: FormGroup = new FormGroup({
+    color: new FormControl(null, [Validators.required]),
+    username: new FormControl(null, [Validators.required], [this.validateUsername]),
+  });
+
+  public validateUsername(control: AbstractControl): Observable<ValidationErrors | null> {
+    return of(null);
+  }
+}
